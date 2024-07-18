cbbad7ef5018d5d4eb4250bf2504ee2576443583
Raymond Campbell <raymond.campbell07@gmail.com>
2024-07-18 15:37:25 -0500

    configured shell

--- portal-shell-demo/src/app/app-routing.module.ts
+++ portal-shell-demo/src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: AppComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: 'mfe1',
+    loadChildren: () => import('mfe1/Module').then(m => m.AppModule)
+  },
+  {
+    path: 'mfe2',
+    loadChildren: () => import('mfe2/Module').then(m => m.AppModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
