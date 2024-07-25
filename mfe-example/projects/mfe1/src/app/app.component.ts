52bebc87ce8d9060a8a00b52253adc720f341f9a
Raymond Campbell <raymond.campbell07@gmail.com>
2024-07-25 14:57:30 -0500

    Migrated succesfully from Module Federation to Native Federation

--- mfe-example/projects/mfe1/src/app/app.component.ts
+++ mfe-example/projects/mfe1/src/app/app.component.ts
@@ -1,8 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { AuthService } from '@demo/auth';
 
 @Component({
   selector: 'app-root',
-  templateUrl: 'app.component.html'
+  standalone: true,
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  constructor() { }
+
+  auth = inject(AuthService);
+
+  ngOnInit(): void {
+    console.log('userName', this.auth.userName);
+  }
+
+  search(): void {
+    alert('Not implemented in this demo!');
+  }
+
 }
--- mfe-example/projects/shell/src/main.ts
+++ mfe-example/projects/shell/src/main.ts
@@ -1,23 +1,6 @@
-// import('./bootstrap')
-// 	.catch(err => console.error(err));
+import { initFederation } from '@angular-architects/native-federation';
 
-// Ideally, we load the Micro Frontend's remoteEntry.js before Angular bootstraps. This file contains meta data about the Micro Frontend, esp. about its shared dependencies. Knowing about them upfront helps Module Federation to avoid version conflicts
-// import { loadRemoteEntry } from '@angular-architects/module-federation';
-
-// Promise.all([
-//   loadRemoteEntry({
-//     type: 'module',
-//     remoteEntry: 'http://localhost:4201/remoteEntry.js',
-//   }),
-// ])
-//   .catch((err) => console.error('Error loading remote entries', err))
-//   .then(() => import('./bootstrap'))
-//   .catch((err) => console.error(err));
-
-// Using a Registry option: Real world option
-import { loadManifest } from '@angular-architects/module-federation';
-
-loadManifest('assets/mf.manifest.json')
-.catch((err) => console.error('Error loading remote entries', err))
-.then(() => import('./bootstrap'))
-.catch((err) => console.error(err));
+initFederation('/assets/federation.manifest.json')
+  .catch(err => console.error(err))
+  .then(_ => import('./bootstrap'))
+  .catch(err => console.error(err));
